fa1c83807935c620aca0a37b3d6e08812180e29f
javipzv <90279135+javipzv@users.noreply.github.com>
2023-06-19 23:49:29 +0200

    some changes

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from script import 
 
 app = Flask(__name__)
 
 @app.route("/")
 def raiz():
     return render_template('index.html') 
+
+@app.route("/results", methods=['POST', 'GET'])
+def show_results():
+    username = request.form.get('username')
+    print(username)
+    return render_template('result.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
