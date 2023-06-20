c7141e8dcb27dd058b2cb3918961abb2cc2ebec3
javipzv <90279135+javipzv@users.noreply.github.com>
2023-06-21 00:29:25 +0200

    Added some parts of the profile comparation

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
 from flask import Flask, render_template, request
 from scripts.script import info_profile
+from tqdm import tqdm
 import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def raiz():
     return render_template('index.html') 
 
-@app.route("/results", methods=['POST', 'GET'])
+@app.route("/profile", methods=["GET"])
+def write_username():
+    return render_template('profile.html')
+
+@app.route("/profile/stats", methods=['POST', 'GET'])
 def show_results():
     username = request.form.get('username')
     directors, actors = info_profile(username)
-    return render_template('result.html', 
+    return render_template('stats.html', 
                            username=username,
                            cols_dir = directors.columns.values,
                            cols_act = actors.columns.values, 
                            rows_dir = directors.values.tolist(),
                            rows_act = actors.values.tolist(),
                            zip=zip)
+
+@app.route("/compare-profiles", methods=["GET"])
+def compare_profiles():
+    return render_template('compare-profiles.html')
+
+@app.route("/comparation-stats", methods=['POST', 'GET'])
+def comparation():
+    u1 = request.form.get('username1')
+    u2 = request.form.get('username2')
+    return render_template('comparation.html',
+                           username1 = u1,
+                           username2 = u2)
 
 @app.route("/contact", methods=["GET"])
 def contact_me():
