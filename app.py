238499c8ad5347d3edbd554941d8de9000f7d505
javipzv <90279135+javipzv@users.noreply.github.com>
2023-06-21 15:01:19 +0200

    almost finished comparison

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
-from scripts.script import info_profile
-from tqdm import tqdm
-import pandas as pd
+from scripts.script import info_profile, info_profile_aux, compare_profiles, compare_profiles_only_score
 
 app = Flask(__name__)
 
@@ -16,7 +14,7 @@ def write_username():
 @app.route("/profile/stats", methods=['POST', 'GET'])
 def show_results():
     username = request.form.get('username')
-    directors, actors, error = info_profile(username)
+    directors, actors, scores, error = info_profile_aux(username)
     if not error:
         return render_template('stats.html', 
                            username=username,
@@ -29,16 +27,20 @@ def show_results():
     return render_template('profile.html')
 
 @app.route("/compare-profiles", methods=["GET"])
-def compare_profiles():
+def page_compare_profiles():
     return render_template('compare-profiles.html')
 
 @app.route("/comparation-stats", methods=['POST', 'GET'])
-def comparation():
+def show_comparation():
     u1 = request.form.get('username1')
     u2 = request.form.get('username2')
+    df_score = compare_profiles_only_score(u1, u2)
     return render_template('comparation.html',
                            username1 = u1,
-                           username2 = u2)
+                           username2 = u2,
+                           cols = df_score.columns.values,
+                           rows = df_score.values.tolist(),
+                           enumerate=enumerate)
 
 @app.route("/contact", methods=["GET"])
 def contact_me():
